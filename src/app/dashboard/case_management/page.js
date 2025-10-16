4e654c5a1e372bdb42e9c9637b14ad42ddedb5f8
Nikhil Kumar <nikhilsk369@gmail.com>
2025-10-16 20:25:59 +0530

    padding added in main div p-2

--- src/app/dashboard/case_management/page.js
+++ src/app/dashboard/case_management/page.js
@@ -63,7 +63,7 @@ export default function CaseListPage() {
   }, [searchTerm]);
 
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto p-2">
       <h1 className="text-2xl font-semibold mb-6 text-center text-gray-800">
         📂 Case Management
       </h1>
