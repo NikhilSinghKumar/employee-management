8b489587b76b2f12b31be3de68ba95fdff7e17ba
Nikhil Kumar <nikhilsk369@gmail.com>
2025-10-19 15:28:40 +0530

    on mobile incresed - rmvd p-x-6

--- src/app/dashboard/talent_acquisition/job_list/[id]/page.js
+++ src/app/dashboard/talent_acquisition/job_list/[id]/page.js
@@ -45,7 +45,7 @@ export default function JobDetailsPage() {
     );
 
   return (
-    <div className="max-w-4xl mx-auto px-6 p-2">
+    <div className="max-w-4xl mx-auto p-2">
       {/* Back to Listings */}
       <div className="m-2">
         <Link
