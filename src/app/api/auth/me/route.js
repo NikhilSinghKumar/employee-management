ce82b4fd67decd54f137e9af24ee0719aba78c1d
Nikhil Kumar <nikhilsk369@gmail.com>
2025-11-03 21:14:47 +0530

    allowed_section included

--- src/app/api/auth/me/route.js
+++ src/app/api/auth/me/route.js
@@ -5,11 +5,17 @@ import jwt from "jsonwebtoken";
 export async function GET(request) {
   const cookies = request.headers.get("cookie");
   if (!cookies)
-    return NextResponse.json({ success: false, message: "No cookies found" }, { status: 401 });
+    return NextResponse.json(
+      { success: false, message: "No cookies found" },
+      { status: 401 }
+    );
 
   const { token } = parse(cookies);
   if (!token)
-    return NextResponse.json({ success: false, message: "Unauthorized" }, { status: 401 });
+    return NextResponse.json(
+      { success: false, message: "Unauthorized" },
+      { status: 401 }
+    );
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -19,9 +25,13 @@ export async function GET(request) {
         userId: decoded.userId,
         email: decoded.email,
         role: decoded.role,
-      }
+        allowed_sections: decoded.allowedSections,
+      },
     });
   } catch (error) {
-    return NextResponse.json({ success: false, message: "Invalid token" }, { status: 403 });
+    return NextResponse.json(
+      { success: false, message: "Invalid token" },
+      { status: 403 }
+    );
   }
 }
