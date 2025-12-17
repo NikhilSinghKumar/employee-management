54d5d42b1f0c6c0e2ab73920f35d8cff32abe44a
Nikhil Kumar <nikhilsk369@gmail.com>
2025-12-17 21:44:20 +0530

    Shadcn Action buttons

--- src/app/dashboard/operations/all_timesheet/page.js
+++ src/app/dashboard/operations/all_timesheet/page.js
@@ -294,9 +294,30 @@ export default function TimesheetPage() {
                         <td className="px-4 py-2 font-semibold border">
                           {entry.grand_total.toFixed(2)}
                         </td>
-                        <td className="px-4 py-2 space-x-2 border capitalize">
-                          {entry.status}
+                        <td className="px-4 py-2 border text-center capitalize">
+                          <span
+                            className={`px-2 py-1 rounded text-xs font-semibold 
+                            ${
+                              entry.status === "draft" &&
+                              "bg-gray-200 text-gray-700"
+                            }
+                            ${
+                              entry.status === "pending" &&
+                              "bg-yellow-100 text-yellow-700"
+                            }
+                            ${
+                              entry.status === "approved" &&
+                              "bg-green-100 text-green-700"
+                            }
+                            ${
+                              entry.status === "revision_required" &&
+                              "bg-orange-100 text-orange-700"
+                            }`}
+                          >
+                            {entry.status.replace("_", " ")}
+                          </span>
                         </td>
+
                         <td className="px-4 py-2 border text-center">
                           <TimesheetActions
                             clientNumber={entry.client_number}
